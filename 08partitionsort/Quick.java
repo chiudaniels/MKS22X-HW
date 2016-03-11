eb4f39020ff9c9f274c7a3331cc3fc208368d27f
chiudaniels <daniel.sean.chiu@gmail.com>
2016-03-10 23:31:33 -0500

    fixed partitio and select. need to work on sort

--- 08partitionsort/Quick.java
+++ 08partitionsort/Quick.java
@@ -1,9 +1,11 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class Quick{
+    /*
     private static int partition(int[]data, int left, int right){
 	int[]temp = new int[data.length];
-	int randomnum= (int)(Math.random()*(right-left+1));
+	int randomnum= left+(int)(Math.random()*((right-left)+1));
 	int leftcount=0;
 	int rightcount=0;
 	int leftpos=0;
@@ -11,39 +13,69 @@ public class Quick{
 	int length= temp.length-1;
 	int lucky= data[randomnum];
 	System.out.println(lucky);
-	System.out.println(left);
-	System.out.println(right);
 	while (leftcount+rightcount<length){
-	    if (data[lucky]>data[leftcount]){
+	    if (lucky>=data[leftcount]){
 		    temp[leftcount]=data[leftpos];
 		    leftcount++;
 		    leftpos++;
 	    }
-	    else if (data[lucky]<data[leftcount]){
+	    else if (lucky<data[leftcount]){
 		    temp[leftcount]=data[length-rightpos];
 		    leftcount++;
 		    rightpos++;
 	    }
-	    if (data[lucky]>data[length-rightcount]){
+	    if (lucky>data[length-rightcount]){
 		temp[length-rightcount]=data[leftpos];
 		rightcount++;
 		leftpos++;
 	    }
-	    else if (data[lucky]<data[length-rightcount]){
+	    else if (lucky<data[length-rightcount]){
 		temp[length-rightcount]=data[length-rightpos];
 		rightcount++;
 		rightpos++;
 	    }
 	}
-	temp[leftpos]=data[randomnum];
+	System.out.println(Arrays.toString(temp));
+	System.out.println(leftpos);
+	temp[leftpos]=lucky;
 	data=temp;
 	return leftpos-1;
     }
+    */
+    
+    public static void swap(int [] data, int x, int y){
+	int temp;
+	temp = data[x];
+	data[x]=data[y];
+	data[y]=temp;
+    }
 
+    private static int partition(int[]data, int left, int right){
+	int randomnum= left+(int)(Math.random()*((right-left)+1));
+	int pivot= data[randomnum];
+	int wall=left;
+	int currentpos=left;
+	//System.out.println(pivot);
+	swap(data,randomnum,data.length-1);
+	while (currentpos<right){
+	    if (pivot < data[currentpos]){
+		currentpos++;
+	    }
+	    if (pivot>data[currentpos]){
+		swap(data,wall,currentpos);
+		wall++;
+		currentpos++;
+	    }
+	    //System.out.println(Arrays.toString(data));
+	}
+	swap(data,wall,currentpos);
+	return wall;
+    }
+    
     public int quickselect(int []data,int k){
 	return quickselect(data,k,0,data.length-1);
     }
-    
+   
     public int quickselect(int []data,int k,int left, int right){
 	if (left == right){
 	    return data[left];
@@ -60,11 +92,11 @@ public class Quick{
 	}
     }
 
-
     public static void main(String[]args){
 	Quick test= new Quick();
 	int[]x={1,9,5,7,3,2};
-	System.out.println(test.partition(x,0,5));
+	System.out.println(test.quickselect(x,2));
+	//test.quickselect(x,4);
 	System.out.println(Arrays.toString(x));
     }
 
