d6043853b4973e3a9e3481a8bc2bafce6e519183
chiudaniels <daniel.sean.chiu@gmail.com>
2016-03-15 16:35:10 -0400

    fixed stack overflow, but sort does work sometimes now

--- 07partitionsort/Quick.java
+++ 07partitionsort/Quick.java
@@ -56,8 +56,8 @@ public class Quick{
     public void quickSortOld(int []data, int left, int right){
 	if (left < right){
 	    int count = partitionOld(data, left, right);
-	    //System.out.println("count:");
-	    //System.out.println(count);
+	    System.out.println("count:");
+	    System.out.println(count);
 	    quickSort(data,left,count);
 	    quickSort(data,count+1,right);
 	}
@@ -73,6 +73,7 @@ public class Quick{
 	int currentpos=left;
 	int newArrayPosLeft=0;
 	int newArrayPosRight=right-left;
+	System.out.println(pivot);
 	while (currentpos<=right){
 	    if (data[currentpos]<pivot){
 		temp[newArrayPosLeft]=data[currentpos];
@@ -96,33 +97,45 @@ public class Quick{
 	return results;
     }
   
-    public void quickSort(int []data){
+    public static void quickSort(int []data){
 	quickSort(data,0,data.length-1);
     } 
     
-    public void quickSort(int []data, int left, int right){
-	int [] count = partition(data, left, right);
-	if (left < count[0]){
-	    quickSort(data,left,count[0]-1);
+    public static void quickSort(int []data, int left, int right){
+	if (left <= right){
+	    int [] count = partition(data, left, right);
+	    //System.out.println(Arrays.toString(data));
+	    //System.out.println(Arrays.toString(count));
+	    //System.out.println("Left:");
+	    //System.out.println(left);  
+	    //System.out.println("Right:");
+	    //System.out.println(right);
+	    if (count[1] < 3){
+	    	return;
+	    }
+	    if (count[0]-1 > 0){
+		quickSort(data,left,count[0]-1);
+	    }
+	    if (count[1] < right-1){
+		quickSort(data,count[1]+1,right);
+	    }
 	}
-	if (count[1]+1<right){
-	    quickSort(data,count[1]+1,right);
-	}
-	return;
     }
 
 
     public static void main(String[]args){
 	Quick test= new Quick();
 	//int[]x={3,2,1,3,2,1,3,2,1};
-	int[]x={8,7,7,3,3,3,3,1,0};
+	int[]x={8,7,6,5,4,3,2,1};
 	test.quickSort(x);
+	//test.partition(x,0,2);
 	//test.partition(x,3,6);
 	//test.partition(x,5,8);
 	//System.out.println(test.partition(x,2,8));
 	//System.out.println(test.quickselect(x,2));
 	//test.quickselect(x,4);
 	System.out.println(Arrays.toString(x));
+
     }
 
 }
